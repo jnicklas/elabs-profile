106fb08795d173f8e463d2ef148b3212ea380cfb
Thetan the Evil Spirit <thetan@thetan.local>
2009-11-23 17:59:34 +0100

    stuff

--- bash.sh
+++ bash.sh
@@ -1,2 +1,6 @@
-ln -nfs `pwd`/resources/profile ~/.profile
+ln -nfs `pwd`/resources/profile ~/.elabs_profile
+
+echo 'source ~/.elabs_profile' > ~/.bash_profile
+
+mkdir -p ~/Projects
 
--- git.sh
+++ git.sh
@@ -3,3 +3,4 @@
 sudo port install git-core +bash_completion
 
 ln -nfs `pwd`/resources/gitconfig ~/.gitconfig
+ln -nfs `pwd`/resources/gitcompletion ~/.gitcompletion
